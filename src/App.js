fc65b23314bbac772717e183bcfe1939558d7dd8
UBaksa <ujkanovicbakir03@gmail.com>
2023-06-13 23:09:45 +0200

    footer added

--- src/App.js
+++ src/App.js
@@ -5,16 +5,16 @@ import Navbar from "./components/Navbar/Navbar";
 import Sidebar from "./components/Sidebar/Sidebar";
 import { Route } from "react-router-dom";
 import { Router } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <>
+    <div className="App">
       <Navbar />
-      <div className="sidebar">
-        <Sidebar />
-      </div>
+      <div className="sidebar">{/* <Sidebar /> */}</div>
       <AnimePage />
-    </>
+      <Footer />
+    </div>
   );
 }
 export default App;
