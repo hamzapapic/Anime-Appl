55251bb68c50d9fa377e7b572835694d9f805c35
eldinhasanovic <almirhasanovic40@gmail.com>
2023-06-14 19:21:46 +0200

    start mainpagea

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import "./App.css";
 import AnimePage from "./pages/AnimePage/AnimePage";
 import Navbar from "./components/Navbar/Navbar";
 import Sidebar from "./components/Sidebar/Sidebar";
-import { Route } from "react-router-dom";
-import { Router } from "react-router-dom";
+import Homepage from "./pages/Homepage/Homepage";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Navbar />
-      <div className="sidebar">
+      {/* <div className="sidebar">
         <Sidebar />
-      </div>
-      <AnimePage />
+      </div> */}
+      <Routes>
+        <Route path="/" element={<Homepage></Homepage>}></Route>
+        <Route path="/home" element={<AnimePage></AnimePage>}></Route>
+      </Routes>
     </>
   );
 }
